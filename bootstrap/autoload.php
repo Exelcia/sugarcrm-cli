b25470129d9c3bb5482f2819a924a125051fb1b6
Teddy EREMBERT DUBAL <terembertdubal@mpi.grp>
2019-06-27 10:02:03 -0400

    * auto load
    Signed-off-by: Teddy EREMBERT DUBAL <terembertdubal@mpi.grp>

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -3,5 +3,5 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require(__DIR__ . '/../vendor/autoload.php');
 } else {
     #Composer autoload
-    require(__DIR__ . '/../../../vendor/autoload.php');
+    require(__DIR__ . '/../../../autoload.php');
 }
